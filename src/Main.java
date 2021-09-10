45918eb415df1d4ed209e868f1fb280838a8267b
Daria Lukyanova <luk.dyr@yandex.ru>
2021-09-10 11:58:03 +0300

    home

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 220;
+        int ru = 20;
+        int miles = (ticket / ru );
+         System.out.println(miles);
+    }
+}
+
+
